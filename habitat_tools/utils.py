0df1d073780d0816f59a0908b3790617a99fc314
Warren Snowden <warren.snowden@gmail.com>
2022-07-17 22:11:13 +0100

    remove timezone localize for time reading

--- habitat_tools/utils.py
+++ habitat_tools/utils.py
@@ -10,7 +10,6 @@ def localise_tz(type_, obj):
         tz_time = tz.localize(obj.created)
         obj.created = tz_time.astimezone(tz=pytz.timezone("Europe/London"))
     elif type_ == "reading":
-        tz_time = tz.localize(obj.time)
         obj.time = tz_time.astimezone(tz=pytz.timezone("Europe/London"))
     return obj
 
